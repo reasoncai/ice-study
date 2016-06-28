411fd8ae18f599b2702eb8809881ccf9734fa4e0
yvonne <yvonne@10.10.12.90>
2016-06-28 14:15:22 +0800

    fa

--- ICETest/src/com/cai/service/OnlineBookI2.java
+++ ICETest/src/com/cai/service/OnlineBookI2.java
@@ -1,5 +1,30 @@
 package com.cai.service;
 
-public class OnlineBookI2 {
+import com.my.demo.book.Message;
+import com.my.demo.book._OnlineBookDisp;
+
+import Ice.Communicator;
+import Ice.Current;
+import IceBox.Service;
+
+public class OnlineBookI2 extends _OnlineBookDisp implements Service{
+
+	@Override
+	public Message bookTick(Message msg, Current __current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void start(String arg0, Communicator arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
